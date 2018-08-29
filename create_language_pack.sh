6fc0c410f46695b257b13651d6bf4fad29b907da
Ken Koster <n7ipb@wetnet.net>
2018-08-29 16:22:57 -0700

    Added Copyright notice

--- create_language_pack.sh
+++ create_language_pack.sh
@@ -1,3 +1,22 @@
+#!/bin/sh
+#
+# Copyright (C) 2018 Ken Koster - N7IPB
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
 # 
 # Simple script to grab configuration info from a tts config file
 # and create a compressed archive of all the associated
